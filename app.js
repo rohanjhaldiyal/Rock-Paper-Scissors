a6af146eace884f485595b6f082a1b17e5b202db
rohan jhaldiyal <rohanjhaldiyal@gmail.com>
2023-09-06 18:37:51 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const computerChoiceDisplay = document.getElementById('computer-choice')
+
 const userChoiceDisplay = document.getElementById('user-choice')
+
 const resultDisplay = document.getElementById('result')
+
 const possibleChoices = document.querySelectorAll('button')
+
 let userChoice
 let computerChoice
 let result
+
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
