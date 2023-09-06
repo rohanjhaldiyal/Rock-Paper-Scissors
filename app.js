35e5c410f43b8aa75ec2bda44c4673d10cffd7b1
rohan jhaldiyal <rohanjhaldiyal@gmail.com>
2023-09-06 19:48:03 +0530

    initial commit

--- app.js
+++ app.js
@@ -12,24 +12,24 @@ possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('cli
     getResult()
 }))
 function generateComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * 3 + 1)
+    const randomNumber = Math.floor(Math.random() * 3) + 1
     computerChoice = randomNumber === 1 ? "rock" : randomNumber === 2 ? "paper" : "scissors"
     computerChoiceDisplay.innerHTML = computerChoice
 }
 function getResult() {
     if(computerChoice === userChoice)
         result = 'Its a Draw!!!'
-    if(computerChoice === "rock" && userChoice === "Scissors")
+    if(computerChoice === "rock" && userChoice === "scissors")
         result = 'You Lose'
-    if(computerChoice === "rock" && userChoice === "Paper")
+    if(computerChoice === "rock" && userChoice === "paper")
         result = 'You Win'
-    if(computerChoice === "paper" && userChoice === "Scissors")
+    if(computerChoice === "paper" && userChoice === "scissors")
         result = 'You Win'
-    if(computerChoice === "paper" && userChoice === "Rock")
+    if(computerChoice === "paper" && userChoice === "rock")
         result = 'You Lose'
-    if(computerChoice === "scissors" && userChoice === "Rock")
+    if(computerChoice === "scissors" && userChoice === "rock")
         result = 'You Win'
-    if(computerChoice === "scissors" && userChoice === "Paper")
+    if(computerChoice === "scissors" && userChoice === "paper")
         result = 'You Lose'
     resultDisplay.innerHTML = result
 }
